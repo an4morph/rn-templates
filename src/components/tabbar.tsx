9d9b890103d695c4577c729b4cc146372f6437bc
an4morph <avgustin456@gmail.com>
2024-07-29 00:43:11 +0600

    created floated tabbar

--- src/components/tabbar.tsx
+++ src/components/tabbar.tsx
@@ -1,16 +1,23 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import {Dimensions} from 'react-native';
 
 interface Props extends BottomTabBarProps {}
 
 const EXCLUDE_ROUTE_NAMES = ['_sitemap', '+not-found']
+const MARGIN_BOTTOM = 30
+const MARGIN_X = 15
+const FOOTER_HEIGHT = 60
+const FOOTER_WIDTH = Dimensions.get('window').width - (MARGIN_X * 2)
 
 export const MyTabBar = ({ state, descriptors, navigation }: Props) => {
-  console.log(state.routeNames)
   return (
-    <View style={{ flexDirection: 'row' }}>
-      {state.routes.map((route, index) => {
-        if (EXCLUDE_ROUTE_NAMES.includes(route.name)) return null
+    <View style={styles.container}>
+      {state.routes
+        .filter((route) => !EXCLUDE_ROUTE_NAMES.includes(route.name))
+        .map((route, index, routes) => {
+        const isLast = routes.length - 1 === index
+        const isFirst = index === 0
 
         const { options } = descriptors[route.key];
         const label =
@@ -35,21 +42,41 @@ export const MyTabBar = ({ state, descriptors, navigation }: Props) => {
         };
 
         return (
-          <TouchableOpacity
+          <Pressable
             key={route.key}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
             onPress={onPress}
-            style={{ flex: 1, backgroundColor: '#dec6ee' }}
+            style={tabStyles({ isFocused }).tab}
           >
-            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
-              {label}
+            <Text style={{ color: 'white', textAlign: 'center' }}>
+              {typeof label === 'string' ? label : route.name}
             </Text>
-          </TouchableOpacity>
+          </Pressable>
         );
       })}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderRadius: FOOTER_HEIGHT / 2,
+    overflow: 'hidden',
+    backgroundColor: '#ccc',
+    width: Dimensions.get('window').width - (FOOTER_HEIGHT / 2),
+    height: FOOTER_HEIGHT,
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: MARGIN_BOTTOM,
+    left: MARGIN_X,
+  },
+})
+
+const tabStyles = ({ isFocused }: { isFocused: boolean }) => StyleSheet.create({
+  tab: {
+    backgroundColor: isFocused ? '#673ab7' : '#222',
+    flex: 1,
+    justifyContent: 'center',
+  }
+})
