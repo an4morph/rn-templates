912fa1e022f38469553b06e76a362385b4ec1537
an4morph <avgustin456@gmail.com>
2024-07-30 01:06:32 +0600

    knowledge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,33 @@
-import { Tabs } from "expo-router"
-import { MyTabBar } from "../src/components/tabbar"
+import { Tabs } from 'expo-router'
+import { MyTabBar } from '../src/components/tabbar'
+import { Foundation } from '@expo/vector-icons'
 
 const MainLayout = (): JSX.Element => {
   return (
-    <Tabs tabBar={props => <MyTabBar {...props}/>}>
-      <Tabs.Screen name="index" options={{title: 'Home'}}/>
-      <Tabs.Screen name="page1" options={{title: 'Fisrt'}}/>
-      <Tabs.Screen name="page2" options={{title: 'Second'}}/>
+    <Tabs tabBar={(props) => <MyTabBar {...props} />}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home Page',
+          tabBarLabel: 'Home',
+          tabBarIcon: (props) => <Foundation name="home" {...props} />,
+          tabBarLabelPosition: 'below-icon',
+          tabBarBadge: '8',
+        }}
+      />
+      <Tabs.Screen
+        name="page1"
+        options={{
+          title: 'Fisrt',
+        }}
+      />
+      <Tabs.Screen
+        name="page2"
+        options={{
+          title: 'Second Page',
+          tabBarIcon: (props) => <Foundation name="heart" {...props} />,
+        }}
+      />
     </Tabs>
   )
 }
--- src/components/tabbar.tsx
+++ src/components/tabbar.tsx
@@ -1,6 +1,6 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native';
-import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
-import {Dimensions} from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
+import { Dimensions } from 'react-native'
 
 interface Props extends BottomTabBarProps {}
 
@@ -8,53 +8,42 @@ const EXCLUDE_ROUTE_NAMES = ['_sitemap', '+not-found']
 const MARGIN_BOTTOM = 30
 const MARGIN_X = 15
 const FOOTER_HEIGHT = 60
-const FOOTER_WIDTH = Dimensions.get('window').width - (MARGIN_X * 2)
 
 export const MyTabBar = ({ state, descriptors, navigation }: Props) => {
   return (
     <View style={styles.container}>
       {state.routes
         .filter((route) => !EXCLUDE_ROUTE_NAMES.includes(route.name))
-        .map((route, index, routes) => {
-        const isLast = routes.length - 1 === index
-        const isFirst = index === 0
+        .map((route, index) => {
+          const { options } = descriptors[route.key]
+          const { tabBarLabel, title, tabBarIcon, tabBarLabelPosition } = options
+          const label = tabBarLabel || title || route.name
+          const isFocused = state.index === index
+          const icon = tabBarIcon && tabBarIcon({ focused: isFocused, size: 20, color: 'white' })
 
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            })
 
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params)
+            }
           }
-        };
 
-        return (
-          <Pressable
-            key={route.key}
-            onPress={onPress}
-            style={tabStyles({ isFocused }).tab}
-          >
-            <Text style={{ color: 'white', textAlign: 'center' }}>
-              {typeof label === 'string' ? label : route.name}
-            </Text>
-          </Pressable>
-        );
-      })}
+          const tabStyles = getTabStyles({ isFocused, index, tabBarLabelPosition, hasIcon: !!icon })
+
+          return (
+            <Pressable key={route.key} onPress={onPress} style={tabStyles.tab}>
+              {icon}
+              <Text style={tabStyles.text}>{typeof label === 'string' ? label : route.name}</Text>
+            </Pressable>
+          )
+        })}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -63,7 +52,7 @@ const styles = StyleSheet.create({
     borderRadius: FOOTER_HEIGHT / 2,
     overflow: 'hidden',
     backgroundColor: '#ccc',
-    width: Dimensions.get('window').width - (FOOTER_HEIGHT / 2),
+    width: Dimensions.get('window').width - FOOTER_HEIGHT / 2,
     height: FOOTER_HEIGHT,
     display: 'flex',
     justifyContent: 'center',
@@ -73,10 +62,30 @@ const styles = StyleSheet.create({
   },
 })
 
-const tabStyles = ({ isFocused }: { isFocused: boolean }) => StyleSheet.create({
-  tab: {
-    backgroundColor: isFocused ? '#673ab7' : '#222',
-    flex: 1,
-    justifyContent: 'center',
-  }
-})
+const getTabStyles = ({
+  isFocused,
+  index,
+  tabBarLabelPosition,
+  hasIcon,
+}: {
+  isFocused: boolean
+  index: number
+  tabBarLabelPosition?: 'beside-icon' | 'below-icon'
+  hasIcon: boolean
+}) =>
+  StyleSheet.create({
+    tab: {
+      backgroundColor: isFocused ? '#673ab7' : '#222',
+      flex: 1,
+      flexDirection: tabBarLabelPosition === 'below-icon' ? 'column' : 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderLeftWidth: index === 0 ? 0 : 1,
+      borderLeftColor: 'white',
+      paddingHorizontal: 10,
+    },
+    text: {
+      color: 'white',
+      marginLeft: hasIcon ? (tabBarLabelPosition === 'below-icon' ? 0 : 10) : 0,
+    },
+  })
